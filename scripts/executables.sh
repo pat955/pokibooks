3220dfbc240d36ccf7c1df8b4c1566e67653eec2
pat955 <patricija.pivoraite@gmail.com>
2024-08-09 08:16:58 +0200

    mac change

--- scripts/executables.sh
+++ scripts/executables.sh
@@ -10,11 +10,3 @@ docker run \
   --env SPECFILE=./PokiBooksLinux.spec \
   batonogov/pyinstaller-linux:latest
 
-docker run -it \
-  --device /dev/kvm \
-  -p 50922:10022 \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -e "DISPLAY=${DISPLAY:-:0.0}" \
-  -v "$(pwd)/build_macos.sh:/build_macos.sh" \
-  -v "$(pwd):/src" \
-  sickcodes/docker-osx:auto /bin/bash /build_macos.sh
